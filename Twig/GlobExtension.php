8502eba0a865befb7edb77f7443ba6a1550ad8b3
Szabolcs Surányi <szabolcs.suranyi@shadesoft.hu>
2020-02-05 00:02:29 +0100

    Update GlobExtension.php

--- Twig/GlobExtension.php
+++ Twig/GlobExtension.php
@@ -2,46 +2,39 @@
 
 namespace ShadeSoft\Twig;
 
-class GlobExtension extends \Twig_Extension {
+class GlobExtension extends \Twig\Extension\AbstractExtension {
 
     /**
      * @return array
      */
     public function getFilters() {
         return array(
-            new \Twig_SimpleFilter('glob', array($this, 'glob'))
-        );
-    }
+            new \Twig\TwigFilter('glob', function($pattern, $returnMatch = true) {
+                $rootPath = $_SERVER['DOCUMENT_ROOT'];
 
-    /**
-     * @param string $pattern
-     * @param bool $returnMatch
-     * @return array
-     */
-    public function glob($pattern, $returnMatch = true) {
-        $rootPath = $_SERVER['DOCUMENT_ROOT'];
-
-        if($returnMatch) {
-            $xPattern = explode('*', $pattern);
-        }
-
-        $results = array();
-        foreach(glob(str_replace('//', '/', "{$rootPath}/{$pattern}")) as $item) {
-            $item = str_replace($rootPath, '', $item);
-
-            if($returnMatch) {
-                $match = $item;
-                foreach($xPattern as $part) {
-                    $match = str_replace($part, '', $match);
+                if($returnMatch) {
+                    $xPattern = explode('*', $pattern);
                 }
 
-                $results[$match] = $item;
-            } else {
-                $results[] = $item;
-            }
-        }
+                $results = array();
+                foreach(glob(str_replace('//', '/', "{$rootPath}/{$pattern}")) as $item) {
+                    $item = str_replace($rootPath, '', $item);
 
-        return $results;
+                    if($returnMatch) {
+                        $match = $item;
+                        foreach($xPattern as $part) {
+                            $match = str_replace($part, '', $match);
+                        }
+
+                        $results[$match] = $item;
+                    } else {
+                        $results[] = $item;
+                    }
+                }
+
+                return $results;
+            })
+        );
     }
 
     public function getName() {
